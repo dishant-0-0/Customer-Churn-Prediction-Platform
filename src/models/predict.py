05591845481108a60c2ce4c9d3c208b5c1597562
Dishant <dishantpatel098@gmail.com>
2026-08-07 12:44:00 +0530

    prediction enhancements

--- src/models/predict.py
+++ src/models/predict.py
@@ -10,12 +10,20 @@ from src.utils.logger import get_logger
 
 logger = get_logger(__name__)
 
+def _prepare_features(
+    preprocessor: ColumnTransformer,
+    X: pd.DataFrame,
+):
+    """
+    Transform raw features using the fitted preprocessor.
+    """
+
+    return preprocessor.transform(X)
+
 def predict(
     model: BaseEstimator,
     preprocessor: ColumnTransformer,
     X: pd.DataFrame,
-    *,
-    return_proba: bool = False,
 ):
     """
     Generate predictions for unseen data.
@@ -23,16 +31,35 @@ def predict(
 
     logger.info("Generating predictions using %s", model.__class__.__name__)
 
-    X_processed = preprocessor.transform(X)
-
-    if return_proba:
-        if not hasattr(model, "predict_proba"):
-            raise AttributeError(
-                f"{model.__class__.__name__} does not implement 'predict_proba()'."
-            )
-
-        return model.predict_proba(
-            X_processed,
-        )[:1]
+    X_processed = _prepare_features(
+        preprocessor=preprocessor,
+        X= X
+    )
 
     return model.predict(X_processed)
+
+def predict_proba(
+    model: BaseEstimator,
+    preprocessor: ColumnTransformer,
+    X: pd.DataFrame
+):
+    """
+    Generate prediction probabilities for unseen data.
+    """
+
+    logger.info("Generating prediction probabilities using %s", model.__class__.__name__)
+
+    if not hasattr(model, "predict_proba"):
+        raise AttributeError(
+            f"{model.__class__.__name__} "
+            "does not implement 'predict_proba()'."
+        )
+
+    X_processed = _prepare_features(
+        preprocessor,
+        X
+    )
+
+    return model.predict_proba(
+        X_processed
+    )[:,1]
